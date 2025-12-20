5e291840191f9130e0a71759115bfb11896eff35
bal16 <ahmadhamidbalya@gmail.com>
2025-12-20 14:29:16 +0700

    style: change variable name

--- components/Sidebar.py
+++ components/Sidebar.py
@@ -1,15 +1,15 @@
 import customtkinter as ctk
-from config import COLOR_ACCENT, FONT_SANS_SERIF, LOGO
+from config import COLOR_ACCENT, FONT_SANS_SERIF, ICON_LOGO
 
 class Sidebar(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         
-        self.logo = ctk.CTkLabel(self, text="", image=LOGO, text_color=COLOR_ACCENT)
+        self.logo = ctk.CTkLabel(self, text="", image=ICON_LOGO, text_color=COLOR_ACCENT)
         self.logo.pack(pady=40, padx=20, anchor="w")
         
-        menus = ["➕ Add", "🎵 Music", "📚 Playlist"]
-        for menu in menus:
+        MENUS = ["➕ Add", "🎵 Music", "📚 Playlist"]
+        for menu in MENUS:
             btn = ctk.CTkButton(self, text=menu, 
                                 fg_color="transparent", 
                                 anchor="w", 
